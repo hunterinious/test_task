2a25cbc94e356e0e223c14e9646d93cd906ca644
Vladyslav Nerodenko <v.nerodenko@pnn.in.ua>
2022-01-11 16:02:47 +0200

    static filed dirs added

--- test_task/settings.py
+++ test_task/settings.py
@@ -159,4 +159,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+MEDIA_URL = 'api/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
